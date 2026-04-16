68e43ee32f8ea79cda3001eb718baf38b68d1363
thwap <kuisma.sami@gmail.com>
2026-04-16 13:58:14 +0300

    connect to mud and client side UI, fix on gmcp/iac parsing to actually work

--- extensions.py
+++ extensions.py
@@ -7,11 +7,15 @@ class GMCP:
     IAC = b"\xff"
     DO  = b"\xfd"
     SB  = b"\xfa"
+    WILL = b"\xfb"
     SE  = b"\xf0"
     GMCP = b"\xc9"
-
+    
     @staticmethod
     async def gmcp_order(writer):
+        writer.write(GMCP.IAC + GMCP.DO + GMCP.GMCP) #for some reason icesus did not negotiate with WILL but do.
+        await writer.drain()
+
         modules = [
             "Room.Info 1",
             "Party 1",
--- game.py
+++ game.py
@@ -8,7 +8,7 @@ import json
 import asyncio
 from models import Dawae, Rooms
 import logging
-
+import re
 #  flask logging config 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
@@ -41,13 +41,23 @@ threading.Thread(target=start_mud_loop, daemon=True).start()
 # --- browser connects.
 @socketio.on('connect')
 def handle_connect():
-    sid = request.sid
-    print("connect:",sid)    
-    asyncio.run_coroutine_threadsafe(
-        mud_session(sid),
-        mud_loop
-    )
+    asyncio.run_coroutine_threadsafe(mud_session(request.sid),mud_loop)
 
+async def send_msg(msg, sid):
+    try:
+        writer = mud_connections[sid]['writer']
+        writer.write((msg+"\n").encode(FORMAT))
+        await writer.drain()
+    except Exception as e:
+        print("error while trying to send data to mud:",e)
+    
+
+@socketio.on('msg')
+def msg(data):
+    sid = request.sid
+    if sid in mud_connections:
+        if data["msg"]:
+            asyncio.run_coroutine_threadsafe(send_msg(data["msg"],sid),mud_loop)
 
 
 async def mud_session(sid):
@@ -59,12 +69,88 @@ async def process_session(sid,reader,writer):
     
     await GMCP.gmcp_order(writer)
     
+    STATE_OUTPUT,STATE_IAC, STATE_IAC_2, STATE_GMCP, STATE_GMCP_PREV_WAS_IAC  = 0,1,2,3,4
+    state = STATE_OUTPUT
+
+    text_buffer = bytearray() 
+    gmcp_buffer = bytearray()
+    output=""
+    
     while True:
+        og_data=b""
+        while True:
+            try:
+                
+                #first will try to get much output/other information there parsed inside python before to browser
+                newline_index = text_buffer.find(b'\n')
+                
+                if newline_index != -1:
+                    og_data = text_buffer[:newline_index]
+                    del text_buffer[:newline_index + 1]
+                    og_data+=b'\n'
+                    #outerloop will handle data and prevent too much nesting.
+                    break 
+                if output:
+                    
+                    socketio.emit('output',{'output':output},to=sid)
+                output = ""
+                chunk = await asyncio.wait_for(reader.read(2048), timeout=0.25)
+                if not chunk:
+                    raise asyncio.CancelledError
+                
+                for b in chunk:
+                    if state == STATE_OUTPUT:
+                        if b == 0xFF:
+                           print("IAC")
+                           state = STATE_IAC
+                        else:
+                           text_buffer.append(b)
+                    elif state == STATE_IAC:
+                        if b == 0xFA:
+                           state = STATE_GMCP
+                        elif b in (0xFB, 0xFC, 0xFD, 0xFE): # WILL/WONT/DO/DONT → 3-byte
+                           state = STATE_IAC_2
+                        elif b in (0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xF0):# 2-byte IAC commands → just skip
+                           state = STATE_OUTPUT
+                        else:
+                           print("something possibly went wrong with happened parsing telnet.")
+                    elif state == STATE_IAC_2:
+                       state = STATE_OUTPUT
+                    elif state == STATE_GMCP:
+                       gmcp_buffer.append(b)
+                       if b ==  0xFF:
+                           state = STATE_GMCP_PREV_WAS_IAC
+                    elif state == STATE_GMCP_PREV_WAS_IAC:
+                       gmcp_buffer.append(b)
+                       if b == 0xF0:
+                           print("GMCP")
+                           if gmcp_buffer.startswith(b'\xc9Party.Info'):
+                               print("PARTYSTATUS")
+                           elif gmcp_buffer.startswith(b'\xc9Room.Info'):
+                               print("ROOMINFO")
+                           state = STATE_OUTPUT
+                           gmcp_buffer.clear()
+                       elif b ==  0xFF:
+                           state = STATE_GMCP_PREV_WAS_IAC 
+                       else:
+                           state = STATE_GMCP
+                
+            # timeout happens if no messages from mud, and since parcing happens only at end of line commands this will flush all not \n terminated in a bit.
+            # (assumption that if went to timeout, there is no telnet signals inside of it.)
+            except asyncio.TimeoutError:
+                if text_buffer:
+                    og_data = bytes(text_buffer)
+                    text_buffer.clear()
+                    break
+            except Exception as e:
+                print("connection error:",e)
         
-        chunk = await asyncio.wait_for(reader.read(2048), timeout=0.25)
-        if not chunk:
-            raise asyncio.CancelledError     
-    #    print(chunk)
+        data = og_data.decode(FORMAT, errors='ignore')
+        #remove unprintables, but leave tab and space.
+        
+        output+=data
+        
+    
 
 @app.route("/")
 def index():
